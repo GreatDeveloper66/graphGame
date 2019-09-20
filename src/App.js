8b78e44e6dd0fb7a60006bfcdfd7c177cf835c0e
Adam <adam@mac.attlocal.net>
2019-09-20 14:03:49 -0500

    Add form functionality

--- src/App.js
+++ src/App.js
@@ -17,23 +17,33 @@ class WelcomeScreen extends Component {
 	constructor(props) {
 		super(props);
 		this.handleSubmit = this.handleSubmit.bind(this);
+  this.handleChange = this.handleChange.bind(this);
 		this.state = {
 			input: '',
 			submit: ''
 		};
 	}
+ handleChange(event){
+   this.setState({
+     input: event.target.value
+   });
+ }
 	handleSubmit(event) {
+  event.preventDefault();
 		this.setState({
-			input: event.target.value
+			input: event.target.value,
+   submit: this.state.input
 		});
 	}
 	render() {
 		return ( <div className = "App">
-															<form>
+															<form onSubmit={this.handleSubmit}>
 																<label> Enter number of nodes you want </label> 
-																		<input type = "text" placeholder = "7" ></input> 
+																		<input type = "text" placeholder = "7" value={this.state.input} onChange={this.handleChange} ></input> 
+  												   <button> Click for Random Nodes </button> 
   												</form> 
-  												<button> Click for Random Nodes </button> 
+  												<br></br>
+  												<h1>{this.state.submit}</h1>
   									</div>
 		       );
 	     }
