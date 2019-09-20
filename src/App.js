1a44275e05df99e422081208cd04fe7d560f107c
Adam <adam@mac.attlocal.net>
2019-09-19 19:36:51 -0500

    Add welcome screen

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-/*global document*/
-/*global Modernizr*/
-/*global SmoothScroll*/
-/*global console*/
-/*global $*/
-/*jshint esversion:6*/
-/*global event*/
-/*global requestAnimationFrame*/
-/*import smoothScroll from '/js/scroll.js';*/
-
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-							<button>Click Here for Graph</button>
-							<table>
-								
-				
-				   </table>
-    
-    </div>
-  );
+class App extends Component {
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return(<WelcomeScreen />);
+	}
 }
+
+const Screen = () => (<div>hello</div>);
+
+class WelcomeScreen extends Component {
+	constructor(props) {
+		super(props);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.state = {
+			input: '',
+			submit: ''
+		};
+	}
+	handleSubmit(event) {
+		this.setState({
+			input: event.target.value
+		});
+	}
+	render() {
+		return ( <div className = "App">
+															<form>
+																<label> Enter number of nodes you want </label> 
+																		<input type = "text" placeholder = "7" ></input> 
+  												</form> 
+  												<button> Click for Random Nodes </button> 
+  									</div>
+		       );
+	     }
+    }
 
 export default App;
